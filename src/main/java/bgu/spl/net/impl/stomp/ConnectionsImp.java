e57026e9d0a397907a6f365442e65d245c40e050
yaelgeis <yaeli21995@gmail.com>
2020-01-06 19:54:30 +0200

    hii

--- src/main/java/bgu/spl/net/impl/stomp/ConnectionsImp.java
+++ src/main/java/bgu/spl/net/impl/stomp/ConnectionsImp.java
@@ -2,19 +2,24 @@ package bgu.spl.net.impl.stomp;
 import bgu.spl.net.srv.ConnectionHandler;
 import bgu.spl.net.srv.Connections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+
 public class ConnectionsImp<T> implements Connections<T> {
 
-    private HashMap<Integer, ConnectionHandler<T>> connectionHandlerConcurrentHashMap = new HashMap<>();
+    private HashMap<Integer, ConnectionHandler<T>> connectionHandlerConcurrentHashMap;
 
     private HashMap<String, String> users;
     private HashMap<String, Boolean> loggedUsers;
+    private HashMap<String, LinkedList<String>> topicList;
 
 
 
-    public ConnectionsImp(HashMap<Integer, ConnectionHandler<T>> connectionHandlerConcurrentHashMap, HashMap<String, String> users, HashMap<String, Boolean> loggedUsers) {
-        this.connectionHandlerConcurrentHashMap = connectionHandlerConcurrentHashMap;
-        this.users = users;
-        this.loggedUsers = loggedUsers;
+    public ConnectionsImp() {
+        connectionHandlerConcurrentHashMap = new HashMap<>();;
+        topicList = new HashMap<>();
+        users = new HashMap<>();
+        loggedUsers = new HashMap<>();
     }
 
 
